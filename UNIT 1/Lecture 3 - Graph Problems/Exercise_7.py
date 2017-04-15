6b8efa7a6332fadccf10557610be40f7ca1b1589
Arjun Narayanan <anarayanan86@gmail.com>
2017-04-14 19:07:05 -0700

    Update Exercise_7.py

--- UNIT 1/Lecture 3 - Graph Problems/Exercise_7.py
+++ UNIT 1/Lecture 3 - Graph Problems/Exercise_7.py
@@ -2,9 +2,9 @@
 
 # Exercise 7
 # 10/10 points (graded)
-# Consider once again our permutations of students in a line. Recall the nodes in the graph represent permutations, and that the edges represent
-# swaps of adjacent students. We want to design a weighted graph, weighting edges higher for moves that are harder to make. Which of these could be
-# easily implemented by simply assigning weights to the edges already in the graph?
+# Consider once again our permutations of students in a line. Recall the nodes in the graph represent permutations, and that the edges
+# represent swaps of adjacent students. We want to design a weighted graph, weighting edges higher for moves that are harder to make.
+# Which of these could be easily implemented by simply assigning weights to the edges already in the graph?
 
 # A) A large student who is difficult to move around in line.
 # B) A sticky spot on the floor which is difficult to move onto and off of.
@@ -12,8 +12,9 @@
 # correct
 
 
-# Write a WeightedEdge class that extends Edge. Its constructor requires a weight parameter, as well as the parameters from Edge. You should
-# additionally include a getWeight method. The string value of a WeightedEdge from node A to B with a weight of 3 should be "A->B (3)".
+# Write a WeightedEdge class that extends Edge. Its constructor requires a weight parameter, as well as the parameters from Edge. You
+# should additionally include a getWeight method. The string value of a WeightedEdge from node A to B with a weight of 3 should be
+# "A->B (3)".
 
 # class WeightedEdge(Edge):
 #     def __init__(self, src, dest, weight):
