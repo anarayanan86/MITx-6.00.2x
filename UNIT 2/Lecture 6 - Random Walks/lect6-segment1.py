c6b6ff1d262c676202ee2fba29af01abcfa406be
Arjun Narayanan <anarayanan86@gmail.com>
2017-04-26 22:18:30 -0700

    Create lect6-segment1.py

---
+++ UNIT 2/Lecture 6 - Random Walks/lect6-segment1.py
@@ -0,0 +1,65 @@
+import random
+
+class Location(object):
+    def __init__(self, x, y):
+        """x and y are numbers"""
+        self.x = x
+        self.y = y
+
+    def move(self, deltaX, deltaY):
+        """deltaX and deltaY are numbers"""
+        return Location(self.x + deltaX, self.y + deltaY)
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def distFrom(self, other):
+        ox = other.x
+        oy = other.y
+        xDist = self.x - ox
+        yDist = self.y - oy
+        return (xDist**2 + yDist**2)**0.5
+
+    def __str__(self):
+        return '<' + str(self.x) + ', ' + str(self.y) + '>'
+
+class Field(object):
+    def __init__(self):
+        self.drunks = {}
+        
+    def addDrunk(self, drunk, loc):
+        if drunk in self.drunks:
+            raise ValueError('Duplicate drunk')
+        else:
+            self.drunks[drunk] = loc
+            
+    def moveDrunk(self, drunk):
+        if drunk not in self.drunks:
+            raise ValueError('Drunk not in field')
+        xDist, yDist = drunk.takeStep()
+        currentLocation = self.drunks[drunk]
+        #use move method of Location to get new location
+        self.drunks[drunk] = currentLocation.move(xDist, yDist)
+        
+    def getLoc(self, drunk):
+        if drunk not in self.drunks:
+            raise ValueError('Drunk not in field')
+        return self.drunks[drunk]
+
+class Drunk(object):
+    def __init__(self, name = None):
+        """Assumes name is a str"""
+        self.name = name
+
+    def __str__(self):
+        if self != None:
+            return self.name
+        return 'Anonymous'
+
+class UsualDrunk(Drunk):
+    def takeStep(self):
+        stepChoices = [(0,1), (0,-1), (1, 0), (-1, 0)]
+        return random.choice(stepChoices)
